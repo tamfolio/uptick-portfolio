b61776e1f0f2186625fa984a8f8a36e7da88c107
Tamilore <tamiloreolaniyi@gmail.com>
2023-09-24 19:20:33 +0100

    push

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { FaFacebook, FaTwitter ,FaLinkedinIn} from "react-icons/fa";
 
 function Footer() {
   return (
-    <div className="w-[100%] h-[30vh] bg-white flex flex-col md:flex-row items-center justify-between px-[200px] py-[20px] text-black">
+    <div className="w-[100%] h-[30vh] bg-white flex flex-col md:flex-row items-center justify-between px-[200px] py-[20px] text-black" id="contact">
       <h1 className="font-bold text-[32px] ">PortFolio</h1>
       <div className="flex gap-[20px] md:gap-[50px]">
         <a className="text-primary font-semibold text-[16px]" href="#home">
@@ -14,7 +14,7 @@ function Footer() {
         <a className="text-primary font-semibold text-[16px]" href="#about">
           About
         </a>
-        <span className="text-primary font-semibold text-[16px]" href="contact">
+        <span className="text-primary font-semibold text-[16px]" href="#contact">
           Contact
         </span>
       </div>
--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,18 +1,35 @@
 import {GiHamburgerMenu} from 'react-icons/gi'
+import {FaRegWindowClose} from 'react-icons/fa'
+import { useState } from 'react'
 
 function Navbar() {
+    const [showMenu, setShowMenu] = useState(false);
+    const toggleMenu = () => {
+        setShowMenu(!showMenu)
+    }
   return (
-    <div className='w-[100%] h-[50px] flex items-center justify-between pt-[54px] mb-[50px] px-[30px] md:px-[100px]'>
+    <div className='w-[100%] h-[50px] flex items-center justify-between pt-[54px] mb-[50px] px-[30px] md:px-[100px] relative'>
       <h1>Port<span>Folio</span></h1>
       <div className="gap-[50px] hidden md:flex">
         <a className="text-primary font-semibold text-[16px]" href="#home">Home</a>
         <a className="text-primary font-semibold text-[16px]" href="#work">Work</a>
         <a className="text-primary font-semibold text-[16px]" href="#about">About</a>
-        <span className="text-primary font-semibold text-[16px]" href="contact">Contact</span>
+        <span className="text-primary font-semibold text-[16px]" href="#contact">Contact</span>
       </div>
-      <div className='md:hidden'>
+      <div className='md:hidden' onClick={toggleMenu}>
           <GiHamburgerMenu/>
       </div>
+      {showMenu && <div className='w-[100%] h-[100vh] bg-black absolute top-0 z-10 pt-[30px] px-[30px]'>
+        <div className='flex w-[100%] items-center justify-end' onClick={toggleMenu}>
+        <FaRegWindowClose className='fill-white w-[30px] h-[30px]'/> 
+        </div>
+        <div className="gap-[30px] flex flex-col mt-[50px] px-[50px]">
+        <a className="text-white font-semibold text-[16px]" href="#home" onClick={toggleMenu}>Home</a>
+        <a className="text-white font-semibold text-[16px]" href="#work" onClick={toggleMenu}>Work</a>
+        <a className="text-white font-semibold text-[16px]" href="#about" onClick={toggleMenu}>About</a>
+        <span className="text-white font-semibold text-[16px]" href="#contact" onClick={toggleMenu}>Contact</span>
+      </div>
+        </div>}
     </div>
   )
 }
